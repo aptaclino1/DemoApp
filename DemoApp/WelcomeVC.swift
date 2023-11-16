d5812d03c6828bb77280525896494ed45c9c081e
aptaclino1 <aptaclino.8@gmail.com>
2023-11-17 00:08:12 +0800

    add label

--- DemoApp/WelcomeVC.swift
+++ DemoApp/WelcomeVC.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        textLabel.text = "Hello world"
+        textLabel.textColor = .black
+        textLabel.textAlignment = .center
+        
+        
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
